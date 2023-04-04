b0095690020f162bdf55a6ca0d9760796e97888c
Björn <bjorn@ringmann.se>
2023-04-04 15:47:43 +0200

    Indent properly

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const readline = require('readline')
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 })
 
 const closeReadline = () => {
@@ -12,25 +12,25 @@ const closeReadline = () => {
 
 const getFieldSize = () => {
   return new Promise((resolve, reject) => {
-      rl.question(`Enter field size [X Y]: `, (fieldSizeInput) => {
-        resolve(fieldSizeInput)
-      })
+    rl.question(`Enter field size [X Y]: `, (fieldSizeInput) => {
+      resolve(fieldSizeInput)
+    })
   })
 }
 
 const getStartPosition = () => {
   return new Promise((resolve, reject) => {
-      rl.question(`Enter start position [X Y Compass direction (N E S W)]: `, (startPositionInput) => {
-        resolve(startPositionInput)
-      })
+    rl.question(`Enter start position [X Y Compass direction (N E S W)]: `, (startPositionInput) => {
+      resolve(startPositionInput)
+    })
   })
 }
 
 const getMovements = () => {
   return new Promise((resolve, reject) => {
-      rl.question(`Enter movements in a single row (L = Left, R = Right, F = Move in set direction): `, (movements) => {
-        resolve(movements)
-      })
+    rl.question(`Enter movements in a single row (L = Left, R = Right, F = Move in set direction): `, (movements) => {
+      resolve(movements)
+    })
   })
 }
 
@@ -43,19 +43,19 @@ const go = (size, startPosition, movements) => {
       let x = parseInt(position[0]);
       let y = parseInt(position[1]);
       let direction = position[2].toUpperCase();
-    
+
       // Turn to the left
       const goLeft = () => {
         const index = directions.indexOf(direction);
         direction = directions[(index + 3) % 4];
       }
-    
+
       // Turn to the right
       const goRight = () => {
         const index = directions.indexOf(direction);
         direction = directions[(index + 1) % 4];
       }
-    
+
       // 1 step forward
       const goForward = () => {
         if (direction === "N") {
@@ -68,10 +68,10 @@ const go = (size, startPosition, movements) => {
           x = Math.max(x - 1, 0); // Either subtract or set to edge (0)
         }
       }
-    
+
       // Iterate moves
       for (let i = 0; i < movements.length; i++) {
-        (movements[i].toUpperCase() === "L") && goLeft(); 
+        (movements[i].toUpperCase() === "L") && goLeft();
         (movements[i].toUpperCase() === "R") && goRight();
         (movements[i].toUpperCase() === "F") && goForward();
       }
